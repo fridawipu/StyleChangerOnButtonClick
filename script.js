56230fbcae0193bbaa23841d0aea4c83627c819c
Lloyd Grave <122023696+fridawipu@users.noreply.github.com>
2023-12-06 23:06:12 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('changeStyle').addEventListener('click', function() {
+    var styledElement = document.getElementById('styledElement');
+    styledElement.style.color = 'red';
+    styledElement.style.fontSize = '20px';
+    styledElement.style.border = '2px solid blue';
+});
